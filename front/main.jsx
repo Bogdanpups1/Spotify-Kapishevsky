3470d2a9cce146f4f2429389433228c7a66d6c93
name <emeil@gmail.com>
2026-04-30 13:33:22 +0300

    123

--- front/main.jsx
+++ front/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Error from "./src/components/pages/error404/error";
+import Setting from "./src/components/lending/seting.jsx";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="*" element={<Error />} />
+          <Route path="/setting" element={<Setting />} />
         </Routes>
       </BrowserRouter>
   </StrictMode>
--- front/src/components/header/header.jsx
+++ front/src/components/header/header.jsx
@@ -4,49 +4,18 @@ import SearchBar from './search';
 
 export const Header_bar = () => {
   return (
-    <div style={{ 
-      width: 'auto', 
-      height: 64, 
-      paddingLeft: 37, 
-      paddingRight: 37, 
-      background: 'rgba(0, 18, 33, 0.80)', 
-      borderBottom: '0.50px #A6DAFF solid', 
-      justifyContent: 'space-between', 
-      alignItems: 'center', 
-      display: 'inline-flex' 
-    }}>
-
-      <div style={{ 
-        paddingLeft: 16, 
-        paddingRight: 16, 
-        justifyContent: 'flex-start', 
-        alignItems: 'center', 
-        gap: 20, 
-        display: 'flex', 
-        flexWrap: 'wrap', 
-        alignContent: 'center' 
-      }}>
+    <div className={styles.header}>
+      <div className={styles.logoContainer}>
         <img src="././src/components/header/img/logo.svg" alt="logo" />
       </div>
 
+      
       <SearchBar />
 
-      <div style={{ 
-        width: 119, 
-        height: 56, 
-        paddingLeft: 10, 
-        paddingRight: 10, 
-        paddingTop: 7, 
-        paddingBottom: 7, 
-        borderRadius: 40, 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        gap: 5, 
-        display: 'flex' 
-      }}>
-        <div style={{ justifyContent: 'flex-start', alignItems: 'center', gap: 16, display: 'flex' }}>
-          <div data-property-1="выкл" style={{ padding: 7, justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex' }}>
-            <div style={{ width: 26, height: 28, position: 'relative', overflow: 'hidden' }} />
+      <div className={styles.rightBlock}>
+        <div className={styles.iconsWrapper}>
+          <div data-property-1="выкл" className={styles.iconPlaceholder}>
+            <div className={styles.iconPlaceholderInner} />
           </div>
           <img src="././src/components/header/img/msg.svg" alt="logo" />
           <img src="././src/components/header/img/user.svg" alt="logo" />
